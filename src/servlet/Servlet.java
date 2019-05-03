57fab535c857854401860e2610ea2fd5cbc9bbd5
Administrator <https://github.com/wlyxtx/IDEA.git>
2019-05-03 09:40:25 +0800

    Initial commit

---
+++ src/servlet/Servlet.java
@@ -0,0 +1,15 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet2", urlPatterns = "/Servlet")
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().println("Upload GitHub Test");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request, response);
+    }
+}
