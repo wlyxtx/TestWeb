1686ff38d5c310fa286d3c9443014e084468d183
Administrator <https://github.com/wlyxtx/IDEA.git>
2019-05-03 10:00:21 +0800

    test

--- src/servlet/ServletTest.java
+++ src/servlet/ServletTest.java
@@ -3,8 +3,8 @@ package servlet;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-@WebServlet(name = "ServletOne", urlPatterns = "/ServletOne")
-public class ServletOne extends javax.servlet.http.HttpServlet {
+@WebServlet(name = "ServletTest", urlPatterns = "/ServletTest")
+public class ServletTest extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         response.getWriter().println("Upload GitHub Test");
     }
